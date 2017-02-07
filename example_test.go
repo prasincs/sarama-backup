e94a21f344a934c55262657cd337f1f85e4958ea
Nicolas S. Dade <nic.dade@gmail.com>
2017-02-07 12:51:04 -0800

    update example code

--- example_test.go
+++ example_test.go
@@ -4,23 +4,32 @@
   Copyright 2016 MistSys
 */
 
-package consumer
+package consumer_test
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Shopify/sarama"
+	consumer "github.com/mistsys/sarama-consumer"
+	"github.com/mistsys/sarama-consumer/offsets"
+	"github.com/mistsys/sarama-consumer/stable"
 )
 
 func ExampleNewClient() {
 	// create a suitable sarama.Client
 	sconfig := sarama.NewConfig()
-	sconfig.Version = MinVersion          // consumer requires at least 0.9
+	sconfig.Version = consumer.MinVersion // consumer requires at least 0.9
 	sconfig.Consumer.Return.Errors = true // needed if asynchronous ErrOffsetOutOfRange handling is desired (it's a good idea)
 	sclient, _ := sarama.NewClient([]string{"kafka-broker:9092"}, sconfig)
 
-	// from that, create a consumer.Client with a default configuration
-	client, _ := NewClient("group_name", nil, sclient)
+	// from that, create a consumer.Config with some fancy options
+	config := consumer.NewConfig()
+	config.Partitioner = stable.New(false)                                                 // use a stable (but inconsistent) partitioner
+	config.StartingOffset, config.OffsetOutOfRange = offsets.NoOlderThan(time.Second * 30) // always start and restart no more than 30 seconds in the past (NOTE: requires kafka 0.10 brokers to work properly)
+
+	// and finally a consumer Client
+	client, _ := consumer.NewClient("group_name", config, sclient)
 	defer client.Close() // not strictly necessary, since we don't exit, but this is example code and someone might C&V it and exit
 
 	// consume and print errors
@@ -34,7 +43,7 @@ func ExampleNewClient() {
 	topic_consumer, _ := client.Consume("topic1")
 	defer topic_consumer.AsyncClose() // same comment as for client.Close() above
 
-	// process messages and log errors
+	// process messages
 	for msg := range topic_consumer.Messages() {
 		fmt.Println("processing message", msg)
 		topic_consumer.Done(msg) // required
